3667baa58b9f83544713ab342d66d5a6e745dd9c
Marco Iorio <marco.iorio@polito.it>
2020-10-25 10:13:51 +0100

    Add the Github actions pipelines

---
+++ .github/actions/edit-keycloak-redirect-uri/edit-keycloak-redirect-uris.py
@@ -0,0 +1,42 @@
+import argparse
+import keycloak as kc
+
+if __name__ == "__main__":
+    # Parse the command line arguments
+    parser = argparse.ArgumentParser(description="Update the redirect URIs in keycloak")
+    parser.add_argument("--keycloak-user", help="The admin username for the OIDC server", required=True)
+    parser.add_argument("--keycloak-pass", help="The admin password for the OIDC server", required=True)
+    parser.add_argument("--client-id", help="The ID of the Keycloak client to be edited", required=True)
+    parser.add_argument("--action", choices=['add', 'remove'], help="The action to be performed (add/remove)")
+    parser.add_argument("--redirect-uri", help="The redirect URI to be added/removed", required=True)
+
+    args = parser.parse_args()
+
+    server_url = "https://auth.crownlabs.polito.it/auth/"
+    print(f"Establishing connection to: {server_url}")
+
+    keycloak_admin = kc.KeycloakAdmin(
+        server_url=server_url,
+        username=args.keycloak_user,
+        password=args.keycloak_pass,
+        user_realm_name="master",
+        realm_name="crownlabs",
+        verify=True)
+
+    # Get the client information
+    print("Retrieving client information...")
+    client_info = keycloak_admin.get_client(args.client_id)
+    redirect_uris = set(client_info['redirectUris'])
+
+    if args.action == 'add':
+        print(f"Adding redirect URI: {args.redirect_uri}")
+        redirect_uris.add(args.redirect_uri)
+
+    if args.action == 'remove':
+        print(f"Removing redirect URI: {args.redirect_uri}")
+        redirect_uris.discard(args.redirect_uri)
+
+    print("Updating client information...")
+    client_info['redirectUris'] = list(redirect_uris)
+    client_info = keycloak_admin.update_client(args.client_id, client_info)
+    print("Update completed correctly")
