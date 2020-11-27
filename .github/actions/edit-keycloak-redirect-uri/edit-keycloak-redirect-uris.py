c33dbe28548fbc6a81eebcc2922a05d181875596
Marco Iorio <marco.iorio@polito.it>
2020-11-27 13:50:52 +0100

    Fix the flake8 issues

--- .github/actions/edit-keycloak-redirect-uri/edit-keycloak-redirect-uris.py
+++ .github/actions/edit-keycloak-redirect-uri/edit-keycloak-redirect-uris.py
@@ -1,6 +1,4 @@
 import argparse
-import os
-import sys
 import keycloak as kc
 
 if __name__ == "__main__":
@@ -9,13 +7,12 @@ if __name__ == "__main__":
     parser.add_argument("--keycloak-user", help="The admin username for the OIDC server", required=True)
     parser.add_argument("--keycloak-pass", help="The admin password for the OIDC server", required=True)
     parser.add_argument("--client-id", help="The ID of the Keycloak client to be edited", required=True)
-    parser.add_argument("--action", choices=['add','remove'], help="The action to be performed (add/remove)")
+    parser.add_argument("--action", choices=['add', 'remove'], help="The action to be performed (add/remove)")
     parser.add_argument("--redirect-uri", help="The redirect URI to be added/removed", required=True)
-
 
     args = parser.parse_args()
 
-    server_url="https://auth.crownlabs.polito.it/auth/"
+    server_url = "https://auth.crownlabs.polito.it/auth/"
     print(f"Establishing connection to: {server_url}")
 
     keycloak_admin = kc.KeycloakAdmin(
@@ -27,7 +24,7 @@ if __name__ == "__main__":
         verify=True)
 
     # Get the client information
-    print(f"Retrieving client information...")
+    print("Retrieving client information...")
     client_info = keycloak_admin.get_client(args.client_id)
     redirect_uris = set(client_info['redirectUris'])
 
@@ -39,7 +36,7 @@ if __name__ == "__main__":
         print(f"Removing redirect URI: {args.redirect_uri}")
         redirect_uris.discard(args.redirect_uri)
 
-    print(f"Updating client information...")
+    print("Updating client information...")
     client_info['redirectUris'] = list(redirect_uris)
     client_info = keycloak_admin.update_client(args.client_id, client_info)
-    print(f"Update completed correctly")
+    print("Update completed correctly")
